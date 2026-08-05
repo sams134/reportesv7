ddf60c2139c7561344740a4ba7b5fa6d144c0edc
Samuel Mayorga <Samuel.mayorga@cmeamir.com>
2026-08-05 12:59:22 -0600

    Backup local channels configuration

--- routes/channels.php
+++ routes/channels.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
+
+
+
+
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
@@ -16,3 +20,12 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+
+
+
+
+
+
+
+
